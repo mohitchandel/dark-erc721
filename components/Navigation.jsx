30579dacd754107593ab8970d09d84585e43cecc
Mohit <devmohit93@gmail.com>
2022-10-21 19:38:15 +0530

    first commit

---
+++ components/Navigation.jsx
@@ -0,0 +1,87 @@
+
+export const Navigation= () => {
+    return (
+    
+
+<header className="transparent border-bottom">
+<div className="container">
+<div className="row">
+    <div className="col-md-12">
+        <div className="de-flex sm-pt10">
+            <div className="de-flex-col">
+                <div className="de-flex-col">
+                    <div id="logo">
+                        <a href="dark-index.html">
+                            <img alt="" src="images/logo-light.png" />
+                        </a>
+                    </div>
+                </div>
+                <div className="de-flex-col">
+                    <input id="quick_search" className="xs-hide" name="quick_search" placeholder="search item here..." type="text" />
+                </div>
+            </div>
+            <div className="de-flex-col header-col-mid">
+                <ul id="mainmenu">
+                    <li>
+                        <a href="dark-index.html">Home<span></span></a>
+                        <ul>
+                            <li><a href="dark-index.html">Homepage 1</a></li>
+                            <li><a href="dark-index-2.html">Homepage 2</a></li>
+                            <li><a href="dark-index-3.html">Homepage 3</a></li>
+                        </ul>
+                    </li>                                                                        
+                    <li>
+                        <a href="dark-explore.html">Explore<span></span></a>
+                        <ul>
+                            <li><a href="dark-explore.html">Explore</a></li>
+                            <li><a href="dark-collection.html">Collections</a></li>
+                            <li><a href="dark-item-details.html">Item Details</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="dark-activity.html">Activity<span></span></a>
+                    </li>
+                    <li>
+                        <a href="#">Pages<span></span></a>
+                        <ul>
+                            <li><a href="dark-author.html">Author</a></li>
+                            <li><a href="dark-news.html">News</a></li>
+                            <li><a href="dark-gallery.html">Gallery</a></li>
+                            <li><a href="dark-login.html">Login</a></li>
+                            <li><a href="dark-login-2.html">Login 2</a></li>
+                            <li><a href="dark-register.html">Register</a></li>
+                            <li><a href="dark-contact.html">Contact Us</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Elements<span></span></a>
+                        <ul>
+                            <li><a href="dark-icons-elegant.html">Elegant Icons</a></li>
+                            <li><a href="dark-icons-etline.html">Etline Icons</a></li>
+                            <li><a href="dark-icons-font-awesome.html">Font Awesome Icons</a></li>
+                            <li><a href="dark-accordion.html">Accordion</a></li> 
+                            <li><a href="dark-alerts.html">Alerts</a></li>
+                            <li><a href="dark-counters.html">Counters</a></li>
+                            <li><a href="dark-modal.html">Modal</a></li>
+                            <li><a href="dark-popover.html">Popover</a></li>
+                            <li><a href="dark-pricing-table.html">Pricing Table</a></li>
+                            <li><a href="dark-progress-bar.html">Progress Bar</a></li>
+                            <li><a href="dark-tabs.html">Tabs</a></li>
+                            <li><a href="dark-tooltips.html">Tooltips</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div className="menu_side_area">
+                    <a href="dark-login.html" className="btn-main">Login</a>
+                    <span id="menu-btn"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</header>
+
+    
+)
+}
